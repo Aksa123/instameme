98c984c34154a75f9fafecc0ecb4962708e1699e
Aksa <rapty.here@gmail.com>
2022-05-12 21:12:43 +0700

    refixed react app -> added basename for relative path

--- gameraptor/urls.py
+++ gameraptor/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
-app_name = "instagif"
+
+app_name = "gameraptor"
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', TemplateView.as_view(template_name='index.html'), name="index"),
     path('test', views.test, name="gameraptor_test")
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
